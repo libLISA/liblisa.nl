b62a5818c20a7dfaa8a01829cb56dc86de3ce7eb
Jos Craaijo <git@liblisa.nl>
2026-06-14 17:58:45 +0200

    Inline global css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/global.css'],
   modules: ['@nuxt/content', '@nuxt/icon', 'nuxt-svgo'],
   app: {
     head: {
