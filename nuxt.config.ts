277431c3a2fc3f79944e00373d92685e04913f36
Jos Craaijo <git@liblisa.nl>
2026-06-14 18:10:35 +0200

    Optimize favicons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,14 @@ export default defineNuxtConfig({
     head: {
       htmlAttrs: {
         lang: 'en'
-      }
+      },
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/favicon-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/favicon-512x512.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon-180x180.png' },
+        { rel: 'icon', href: '/favicon.ico' }
+      ]
     }
   },
   icon: {
